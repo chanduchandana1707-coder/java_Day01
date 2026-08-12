8bd4951971b19eb7f9baefd85aa6643a5d668015
chanduchandana1707-coder <chanduchandana1707@gmail.com>
2026-08-12 11:03:56 +0530

    Day01-3rd program

---
+++ program03.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+ 
+public class program03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] names = new String[5];
+
+        System.out.println("Enter 5 names:");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter name" + (i + 1) + ":");
+            names[i] = sc.nextLine();;
+
+        }
+
+        System.out.println("\nThe 5 names are:");
+
+        for (int i = 0; i < 5 ; i++) {
+            System.out.println(names[i]);
+
+        }
+
+        sc.close();
+    }
+}
