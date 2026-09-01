ca4b0b56af58908cff98f70c7636d8beae31ff57
chanduchandana1707-coder <chanduchandana1707@gmail.com>
2026-09-01 23:06:48 +0530

    program24

---
+++ program24.java
@@ -0,0 +1,17 @@
+class MissingNumber {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 5};
+
+        int n = 5;
+
+        int total = n * (n + 1) / 2;
+
+        int sum = 0;
+
+        for (int x : arr) {
+            sum += x;
+        }
+
+        System.out.println("Missing number: " + (total - sum));
+    }
+}
